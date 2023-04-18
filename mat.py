7e768bba966685e1d1c1ae88f5b08eabcebd95b8
21eg107a02 <21eg107a02@anurag.edu.in>
2023-04-18 12:00:13 +0530

    matplot

---
+++ mat.py
@@ -0,0 +1,8 @@
+print("hello")
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+x = np.linespace(0,10,100)
+fig = p;t.figure()
+ax1 = fig.add_subplot(2,2,1)
+ax1.plot(x,sin(x),label = "sin(x)")
